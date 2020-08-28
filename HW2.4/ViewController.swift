750ce3e3a72f3a800ce90ad53d71d029ec7f5e06
Denis Fedorov <poundpig@yandex.ru>
2020-08-28 11:11:17 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by fd on 28.08.2020.
+//  Copyright © 2020 Denis Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
